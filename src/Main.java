f74629581e564edf6a2a3be6403e9a59dfb5f12b
Maria Paula <gomezmariapaula46@gmail.com>
2025-02-27 17:58:19 -0500

    Ejercicio adivinar numero

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,33 @@
+import java.sql.SQLOutput;
 import java.util.Random;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        int rondas, dado1, dado2;
-        int max = 10;
+        int max = 50;
         int min = 1;
-
-        int contdado1=0, contdado2=0;
+        int adivinar;
 
         Random ale = new Random();
-
-        rondas = ale.nextInt((max - min +1)) + min;
-        System.out.println("CANTIDAD DE RONDAS: " + rondas );
-
-        for(int i=1; i<= rondas; i++) {
-            dado1 = ale.nextInt((6 - 1 + 1))+1;
-            contdado1 += dado1;
-
-            dado2 = ale.nextInt((6 - 1 + 1))+1;
-            contdado2 += dado2;
-
-            System.out.println();
-            System.out.print("DADO1: " + dado1 + "");
-
-            System.out.print(" DADO2: " + dado2);
+        adivinar = ale.nextInt((max - min +1))+min;
 
 
+        for (int i= 1; i<=3; i++) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("Ingrese el numero: ");
+            int usuario = teclado.nextInt();
 
+            if (usuario == adivinar) {
+                System.out.println("Usted a adivinado el número");
+            } else if (usuario > adivinar) {
+                System.out.println("El número es menor");
+            } else if (usuario < adivinar) {
+                System.out.println("El número es mayor");
+            }
         }
-        System.out.println();
-        if (contdado1 > contdado2) {
-            System.out.println("El ganador es D1 con " + contdado1);
-        }else{
-            System.out.println("El ganador es D2 con "+ contdado2);
-
-        }
-
-
-
+        System.out.println("El numero era "+ adivinar);
     }
 }
