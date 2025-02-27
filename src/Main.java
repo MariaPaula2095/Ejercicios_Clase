35bcb3dedf80542620e2bd6d28231f66787e5442
Maria Paula <gomezmariapaula46@gmail.com>
2025-02-27 17:20:42 -0500

    Ejercicio1 dados

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,45 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        String animal = "Perro";
-        String tipo;
+        int rondas, dado1, dado2;
+        int max = 10;
+        int min = 1;
 
-        System.out.println("switch clásico:");
-        switch (animal) {
-            case "Perro":
-            case "Gato":
-            case "Conejo":
-                tipo = "Mamífero";
-                break;
-            case "Águila":
-            case "Loro":
-            case "Halcón":
-                tipo = "Ave";
-                break;
-            case "Tiburón":
-            case "Delfín":
-            case "Pez payaso":
-                tipo = "Pez";
-                break;
-            default:
-                tipo = "Desconocido";
+        int contdado1=0, contdado2=0;
+
+        Random ale = new Random();
+
+        rondas = ale.nextInt((max - min +1)) + min;
+        System.out.println("CANTIDAD DE RONDAS: " + rondas );
+
+        for(int i=1; i<= rondas; i++) {
+            dado1 = ale.nextInt((6 - 1 + 1))+1;
+            contdado1 += dado1;
+
+            dado2 = ale.nextInt((6 - 1 + 1))+1;
+            contdado2 += dado2;
+
+            System.out.println();
+            System.out.print("DADO1: " + dado1 + "");
+
+            System.out.print(" DADO2: " + dado2);
+
+
+
         }
-        System.out.println("El " + animal + " es un: " + tipo);
+        System.out.println();
+        if (contdado1 > contdado2) {
+            System.out.println("El ganador es D1 con " + contdado1);
+        }else{
+            System.out.println("El ganador es D2 con "+ contdado2);
 
-        // --- SWITCH MEJORADO (JAVA 14+) ---
-        System.out.println("\nswitch nuevo:");
-        tipo = switch (animal) {
-            case "Perro", "Gato", "Conejo" -> "Mamífero";
-            case "Águila", "Loro", "Halcón" -> "Ave";
-            case "Tiburón", "Delfín", "Pez payaso" -> "Pez";
-            default -> "Desconocido";
-        };
-        System.out.println("El " + animal + " es un: " + tipo);
+        }
+
+
+
     }
 }
